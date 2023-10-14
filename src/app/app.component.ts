aa59926a3e642fc61b9d96600d4048affd1de562
Gabriel <106978269+gabox26@users.noreply.github.com>
2023-10-14 20:50:57 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  datos_storage
   constructor() {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import {IonicStorageModule} from '@ionic/storage-angular';
+import {Drivers} from '@ionic/storage';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 if (environment.production) {
   enableProdMode();
 }
 
+bootstrapApplication(AppComponent, {
+  providers : [{
+    provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    importProvidersFrom(IonicModule.forRoot({})),
+
+    importProvidersFrom(IonicStorageModule.forRoot({
+      name: 'testdb',
+      driverOrder: [Drivers.IndexedDB]
+    })
+    ),
+
+    providesRouter('routes'),
+  
+  ]
+})
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+function providesRouter(routes: any): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
