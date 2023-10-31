0b95d5eee8067442e1fbea92569076f7c0cb6938
socartagena02 <sofiacarcastro@gmail.com>
2023-10-31 15:17:14 -0300

    commit

--- src/app/register/RegisterPage.ts
+++ src/app/register/RegisterPage.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators  } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
@@ -13,73 +13,58 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 export class RegisterPage implements OnInit {
 
   formularioRegistro: FormGroup;
-  
+
 
   constructor(public FormBuilder: FormBuilder,
-    public alertController: AlertController, 
-    private afAuth : AngularFireAuth) {
+    public alertController: AlertController,
+    private afAuth: AngularFireAuth) {
     this.formularioRegistro = this.FormBuilder.group({
-      'nombre': new FormControl ("", Validators.required),
-      'apellido': new FormControl ("", Validators.required),
-      'Nickname' : new FormControl ("", Validators.required),
+      'nombre': new FormControl("", Validators.required),
+      'apellido': new FormControl("", Validators.required),
+      'Nickname': new FormControl("", Validators.required),
       'contraseña': new FormControl("", Validators.required),
-      'repetirPassword': new FormControl ("", Validators.required)
+      'repetirPassword': new FormControl("", Validators.required)
     });
   }
-  
+
   ngOnInit() {
   }
   async guardar() {
+    console.log(formulario);
     var formulario = this.formularioRegistro.value;
+    let usuario = {
+      nombre: formulario.nombre,
+      apellido: formulario.apellido,
+      Nickname: formulario.Nickname,
+      contraseña: formulario.contraseña,
+      repetirpassword: formulario.repetirPassword
+    }
 
-    if (this.formularioRegistro.invalid) {
+    //    
+    if (this.formularioRegistro.valid) {
+
+      const alert = await this.alertController.create({
+        header: 'datos completados',
+        message: 'Vuelve a iniciar sesion',
+        buttons: ['OK'],
+      });
+      var guardar = this.formularioRegistro.value;
+      alert.present();
+      console.log(guardar);
+
+    } else {
       const alert = await this.alertController.create({
         header: 'datos incompletos',
         message: 'tienes que completar el formulario porfavor',
         buttons: ['OK'],
       });
-      await alert.present();
-      return;
-
-      } 
-
-      var usuario = {
-        nombre: formulario.nombre,
-        apellido:formulario.apellido,
-        Nickname:formulario.Nickname,
-        contraseña:formulario.contraseña,
-        repetirpassword:formulario.repetirPassword
-      }
-
-      localStorage.setItem('usuario', JSON.stringify(usuario))
+      alert.present();
 
     }
-  
-  // registrarUsuario() {}
-  
-  // registrar( ) {
-  //     const password = this.formularioRegistro.value.contraseña;
-  //     const repetirPassword = this.formularioRegistro.value.repetirPassword;
-  //     const email = this.formularioRegistro.value.correo; 
 
-  //     if (password !== repetirPassword){
-  //       const alert = this.alertController.create({
-  //         header: 'La contraseña no coincide',
-  //         message: 'Reintente nuevamente.',
-  //         buttons: ['ACEPTAR'],
-  //       });
-  //       alert.then(b => b.present());
-  //       return;
-  //     }
-  
 
-  //   this.afAuth.createUserWithEmailAndPassword(password, email).then((user) =>{
-  //     console.log('Usuario registrado con exito',user);
-      
-  //   }).catch((error) =>{
-  //     console.log('Intente con otro usuario', error);
+    localStorage.setItem('usuario', JSON.stringify(usuario))
 
-  //     return error;
-  //   })
-  // }
+  }
+
 }
